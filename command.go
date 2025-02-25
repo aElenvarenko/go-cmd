674a0f271864726ac9e7ad3809e015b41f6a44fb
Alex <alexelenvarenko@gmail.com>
2025-02-25 09:11:33 +0300

    Add comments

--- command.go
+++ command.go
@@ -1,13 +1,14 @@
 package cmd
 
 type Command struct {
-	name      string
-	alias     string
-	desc      string
-	f         func(cmd *Cmd)
-	arguments []*Argument
+	name      string         // Name
+	alias     string         // Alias
+	desc      string         // Description
+	f         func(cmd *Cmd) // Function
+	arguments []*Argument    // Arguments
 }
 
+// Create new Command instance return pointer to Command
 func NewCommand(name, alias, desc string, f func(cmd *Cmd)) *Command {
 	return &Command{
 		name:      name,
@@ -18,31 +19,37 @@ func NewCommand(name, alias, desc string, f func(cmd *Cmd)) *Command {
 	}
 }
 
+// Set command name return pointer to Command
 func (c *Command) SetName(name string) *Command {
 	c.name = name
 	return c
 }
 
+// Set command alias return pointer to Command
 func (c *Command) SetAlias(alias string) *Command {
 	c.alias = alias
 	return c
 }
 
+// Set command description return pointer to Command
 func (c *Command) SetDesc(desc string) *Command {
 	c.desc = desc
 	return c
 }
 
+// Set command execute function return pointer to Command
 func (c *Command) SetFunc(f func(cmd *Cmd)) *Command {
 	c.f = f
 	return c
 }
 
+// Set command arguments return pointer to Command
 func (c *Command) SetArguments(args []*Argument) *Command {
 	c.arguments = args
 	return c
 }
 
+// Set command arguments values
 func (c *Command) SetArgumentsValues(values []string) {
 	if len(c.arguments) > 0 && len(values) > 0 {
 		for i, arg := range c.arguments {
@@ -53,11 +60,13 @@ func (c *Command) SetArgumentsValues(values []string) {
 	}
 }
 
+// Add argument to command return pointer to Command
 func (c *Command) AddArgument(argument string) *Command {
 	c.arguments = append(c.arguments, NewArgument(argument, ""))
 	return c
 }
 
+// Get command argument by name return pointer to Argument
 func (c *Command) GetArgument(name string) *Argument {
 	if len(c.arguments) > 0 {
 		for _, arg := range c.arguments {
