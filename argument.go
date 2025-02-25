2a99cadce67e1267f699a514dba1daa70a97e700
Alex <alexelenvarenko@gmail.com>
2025-02-25 09:09:42 +0300

    Add comments and setters functions

--- argument.go
+++ argument.go
@@ -1,10 +1,11 @@
 package cmd
 
 type Argument struct {
-	name  string
-	value string
+	name  string // Name
+	value string // Value
 }
 
+// Create new Argument instance and return pointer to Argument
 func NewArgument(name, value string) *Argument {
 	return &Argument{
 		name:  name,
@@ -12,10 +13,24 @@ func NewArgument(name, value string) *Argument {
 	}
 }
 
+// Set argument name return pointer to Argument
+func (a *Argument) SetName(name string) *Argument {
+	a.name = name
+	return a
+}
+
+// Set argument value return pointer to Argument
+func (a *Argument) SetValue(value string) *Argument {
+	a.value = value
+	return a
+}
+
+// Get argument name return string
 func (a *Argument) GetName() string {
 	return a.name
 }
 
+// Get argument value return string
 func (a *Argument) GetValue() string {
 	return a.value
 }
